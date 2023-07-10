2c8caa9c029d1561e00391f2e407c4e1220bec49
Priyanka0613 <95872454+Priyanka0613@users.noreply.github.com>
2023-07-11 01:19:00 +0530

    Delete frontend directory

--- frontend/src/context/useContext.ts
+++
@@ -1,4 +0,0 @@
-import { useContext as useReactContext } from 'react'
-import { Context } from './Provider'
-
-export const useContext = () => useReactContext(Context)
--- frontend/src/lib/utils.ts
+++
@@ -1,3 +0,0 @@
-export const cn = (...classNames: (string | undefined)[]) => {
-  return classNames.filter((c) => c).join(' ')
-}
