1c97662537ff329fa29cefcf0f5ed06447a19150
Priyanka0613 <95872454+Priyanka0613@users.noreply.github.com>
2023-07-11 01:19:42 +0530

    Delete backend directory

--- backend/routers/me.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { deleteMe, editMe, getMe } from '../controllers/meController'
-
-const meRouter = Router()
-
-meRouter.route('/').get(getMe).put(editMe).delete(deleteMe)
-
-export default meRouter
